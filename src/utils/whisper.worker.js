1dc4c71b84e566da0e69c0def06e9a8bbb9df23c
DrAhmedAwni <dr.3aw3aw@gmail.com>
2026-06-07 20:08:56 +0300

    seconds

--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -128,23 +128,26 @@ class GenerationTracker {
 
     chunkCallback(data) {
         this.chunks.push(data)
-        const [text, { chunks }] = this.pipeline.tokenizer._decode_asr(
-            this.chunks,
-            {
-                time_precision: this.time_precision,
-                return_timestamps: true,
-                force_full_sequence: false
-            }
-        )
+        try {
+            const [text, { chunks }] = this.pipeline.tokenizer._decode_asr(
+                this.chunks,
+                {
+                    time_precision: this.time_precision,
+                    return_timestamps: true,
+                    force_full_sequences: true
+                }
+            )
 
-        this.processed_chunks = chunks.map((chunk, index) => {
-            return this.processChunk(chunk, index)
-        })
+            this.processed_chunks = chunks.map((chunk, index) => {
+                return this.processChunk(chunk, index)
+            })
 
-
-        createResultMessage(
-            this.processed_chunks, false, this.getLastChunkTimestamp()
-        )
+            createResultMessage(
+                this.processed_chunks, false, this.getLastChunkTimestamp()
+            )
+        } catch (err) {
+            console.warn('Chunk decode skipped:', err.message)
+        }
     }
 
     getLastChunkTimestamp() {
